986027950018bfdaa4273e4c9721885b510d0540
Balogun Oghenerobo Philip <oghenerobo@gmail.com>
2017-07-18 06:06:16 +0100

    now handles external sdk updated config format

--- index.js
+++ index.js
@@ -2,31 +2,57 @@ const configStore = require("./config-store")
 const configKey = 'sdk-in-use-*'
 
 module.exports = {
-    addConfig: (sdk, config) => {
-        configStore.set(sdk, config)
+    addConfig: (sdk, config, adapter) => {
+        //store config
+        configStore.set(sdk, {
+            config,
+            adapter
+        })
+        //load config for adapter
+        if (adapter === undefined || adapter === null || !adapter) {
+            try {
+                require('./sdk/' + sdk).loadConfig()
+            } catch (ex) {
+                throw new Error("could not load adapter")
+            }
+        } else {
+            configStore.get(sdk).adapter.loadConfig()
+        }
     },
     getConfig: (sdk) => {
         if (sdk && configStore.has(sdk)) return configStore.get(sdk)
         else {
             const _val = {}
             configStore.forEach((value, key) => {
-                _val[key] = value
+                _val[key] = value.config || value
             })
             return _val
         }
     },
     use: (sdk) => {
         configStore.set(configKey, sdk)
-        require('./sdk/'+configStore.get(configKey)).loadConfig()
     },
     getSdkInstance: (sdk) => {
-        return require(sdk).raw()
+        const hasAdapter = configStore.get(sdk).adapter
+        if (hasAdapter === undefined || hasAdapter === null || !hasAdapter) {
+            return require('./sdk/'+sdk).raw()
+        }else{
+            configStore.get(sdk).adapter.raw()
+        }
     },
     send: (data) => {
-        if(configStore.has(configKey)){
-            return require('./sdk/'+configStore.get(configKey)).send(data)
-        }else{
-            return Promise.catch("config has not been loaded for this sdk")
+        if (configStore.has(configKey)) {
+            const sdk = configStore.get(configKey)
+            const hasAdapter = configStore.get(sdk).adapter
+            if (hasAdapter === undefined || hasAdapter === null || !hasAdapter) {
+                console.log("message sent from local sdk")
+                return require('./sdk/' + configStore.get(configKey)).send(data)
+            } else {
+                console.log("message sent from adapter sdk")
+                return configStore.get(sdk).adapter.send(data)
+            }
+        } else {
+            return Promise.catch("No default SDK has been selected for use, \ncall .use('sdk') to select on")
         }
     }
 }
